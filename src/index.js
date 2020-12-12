d9edaf983404d025024c74d03440cd0b5879ca98
Cesar Acual <augustoacual@gmail.com>
2020-12-12 09:46:49 -0600

    useRef - primer uso

--- src/index.js
+++ src/index.js
@@ -1,15 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { MultipleCustomHooks } from './components/03-examples/MultipleCustomHooks';
-// import { CounterApp } from './components/01-useState/CounterApp';
-// import { CounterWithCustomHook } from './components/01-useState/CounterWithCustomHook';
-// import { SimpleForm } from './components/02-useEffect/SimpleForm';
-// import { HookApp } from './HookApp';
-// import { FormWithCustomHook } from './components/02-useEffect/FormWithCustomHook';
-
+import { FocusScreen } from './components/04-useRef/FocusScreen';
 
 ReactDOM.render(
-  <MultipleCustomHooks />,
+  <FocusScreen />,
   document.getElementById('root')
 );
 
