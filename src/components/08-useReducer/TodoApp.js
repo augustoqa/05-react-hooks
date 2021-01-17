5965827fba1cf85a56d6f8d8aa3c225879fb6ca4
Cesar Acual <augustoacual@gmail.com>
2021-01-17 10:08:58 -0600

    Borrar un TODO

--- src/components/08-useReducer/TodoApp.js
+++ src/components/08-useReducer/TodoApp.js
@@ -19,8 +19,21 @@ export const TodoApp = () => {
     localStorage.setItem('todos', JSON.stringify(todos))
   }, [todos])
 
+  const handleDelete = (todoId) => {
+    const action = {
+      type: 'delete',
+      payload: todoId
+    }
+
+    dispatch(action)
+  }
+
   const handleSubmit = (e) => {
     e.preventDefault()
+
+    if (description.trim().length <= 1) {
+      return;
+    }
 
     const newTodo = {
       id: new Date().getTime(),
@@ -50,7 +63,11 @@ export const TodoApp = () => {
                 <p className="text-center">
                   {i + 1} {todo.desc}
                 </p>
-                <button className="btn btn-danger">Borrar</button>
+                <button 
+                  className="btn btn-danger"
+                  onClick={ () => handleDelete(todo.id) }>
+                  Borrar
+                </button>
               </li>
             ))}
           </ul>
