1d3da75642f9c8e27c3848007681be5b1053b59f
Cesar Acual <augustoacual@gmail.com>
2021-01-23 10:36:35 -0600

    CreateContext y useContext

--- src/components/09-useContext/MainApp.js
+++ src/components/09-useContext/MainApp.js
@@ -1,8 +1,17 @@
 import React from 'react'
 import { AppRouter } from './AppRouter'
+import { UserContext } from './UserContext'
 
 export const MainApp = () => {
+  const user = {
+    id: 1234,
+    name: 'Cesar',
+    email: 'cesar@gmail.com'
+  }
+
   return (
-    <AppRouter />
+    <UserContext.Provider value={ user }>
+      <AppRouter />
+    </UserContext.Provider>
   )
 }
