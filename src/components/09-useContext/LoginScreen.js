26bfd716f134796f7729e0373f59723580c9b471
Cesar Acual <augustoacual@gmail.com>
2021-01-23 11:15:51 -0600

    usar useContext para obtener acceso a UserContext y modificarlo

--- src/components/09-useContext/LoginScreen.js
+++ src/components/09-useContext/LoginScreen.js
@@ -1,10 +1,21 @@
-import React from 'react'
+import React, { useContext } from 'react';
+import { UserContext } from './UserContext';
 
 export const LoginScreen = () => {
+  const { setUser } = useContext(UserContext);
+
+  console.log(setUser);
+
   return (
     <div>
       <h1>LoginScreen</h1>
       <hr />
+      <button
+        className='btn btn-primary'
+        onClick={() => setUser({ id: 123, name: 'Cesar' })}
+      >
+        Login
+      </button>
     </div>
-  )
-}
+  );
+};
