73a696a58f90bf820ce94710193ea0356b84769a
Cesar Acual <augustoacual@gmail.com>
2021-05-05 10:32:04 -0600

    Pruebas en el TodoApp

--- src/setupTests.js
+++ src/setupTests.js
@@ -1,7 +1,7 @@
-import Enzyme from 'enzyme';
-import Adapter from 'enzyme-adapter-react-16';
-import {createSerializer} from 'enzyme-to-json';
+import Enzyme from "enzyme";
+import Adapter from "@wojtekmaj/enzyme-adapter-react-17";
+import { createSerializer } from "enzyme-to-json";
 
 Enzyme.configure({ adapter: new Adapter() });
 
-expect.addSnapshotSerializer(createSerializer({mode: 'deep'}));
+expect.addSnapshotSerializer(createSerializer({ mode: "deep" }));
